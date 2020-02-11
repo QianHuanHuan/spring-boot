17c543851bdf59e32d173e7bdcc254f2bde6cb1f
1252173251@qq.com <imissyoukiss6>
2020-02-11 20:17:55 +0800

    SSL 证书

--- src/main/java/com/example/retailers/RetailersApplication.java
+++ src/main/java/com/example/retailers/RetailersApplication.java
@@ -1,8 +1,16 @@
 package com.example.retailers;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.example.retailers.bean")
@@ -11,5 +19,42 @@ public class RetailersApplication {
     public static void main(String[] args) {
         SpringApplication.run(RetailersApplication.class, args);
     }
+
+    /**
+     * it's for set http url auto change to https
+     */
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer(){
+        TomcatEmbeddedServletContainerFactory tomcat=new TomcatEmbeddedServletContainerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");//confidential
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    @Bean
+    public Connector httpConnector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        System.out.println(httpPort);
+        System.out.println(httpsPort);
+        System.out.println("------------------------------------->>>>");
+        return connector;
+    }
+
+    @Value("${server.port}") private Integer httpsPort;
+    @Value("${http.port}") private Integer httpPort;
+
 
 }
--- src/main/java/com/example/retailers/dao/RetailersOrderDao.java
+++ src/main/java/com/example/retailers/dao/RetailersOrderDao.java
@@ -8,26 +8,26 @@ import java.util.List;
 @Mapper
 public interface RetailersOrderDao {
 
-    @Select("SELECT * FROM demo_order WHERE id = #{id}")
+    @Select("SELECT * FROM retailers_order WHERE id = #{id}")
     RetailersOrder find(@Param("id") String id);
 
-    @Select("SELECT * FROM demo_order ")
+    @Select("SELECT * FROM retailers_order ")
     List<RetailersOrder> finds();
 
-    @Insert("INSERT INTO demo_order(id, order_number, user_id, commodity_id, sum, remarks, unit_price) " +
+    @Insert("INSERT INTO retailers_order(id, order_number, user_id, commodity_id, sum, remarks, unit_price) " +
             "VALUES " +
-            "(#{demoOrder.id}, #{demoOrder.orderNumber}, #{demoOrder.userId}, #{demoOrder.commodityId}, #{demoOrder.sum}, #{demoOrder.remarks}, #{demoOrder.unitPrice})")
-    int add(@Param("demoOrder") RetailersOrder demoOrder);
+            "(#{retailersOrder.id}, #{retailersOrder.orderNumber}, #{retailersOrder.userId}, #{retailersOrder.commodityId}, #{retailersOrder.sum}, #{retailersOrder.remarks}, #{retailersOrder.unitPrice})")
+    int add(@Param("retailersOrder") RetailersOrder retailersOrder);
 
-    @Update("UPDATE demo_order " +
+    @Update("UPDATE retailers_order " +
             "SET " +
-            "order_number = #{demoOrder.orderNumber}, user_id = #{demoOrder.userId}, commodity_id = #{demoOrder.commodityId}, " +
-            "sum = #{demoOrder.sum}, remarks = #{demoOrder.remarks}, unit_price = #{demoOrder.unitPrice} " +
-            "WHERE id = #{demoOrder.id}")
-    int update(@Param("demoOrder") RetailersOrder demoOrder);
+            "order_number = #{retailersOrder.orderNumber}, user_id = #{retailersOrder.userId}, commodity_id = #{retailersOrder.commodityId}, " +
+            "sum = #{retailersOrder.sum}, remarks = #{retailersOrder.remarks}, unit_price = #{retailersOrder.unitPrice} " +
+            "WHERE id = #{retailersOrder.id}")
+    int update(@Param("retailersOrder") RetailersOrder retailersOrder);
 
-    @Delete("DELETE demo_order WHERE id = #{demoOrder.id}")
-    int delete(@Param("demoOrder") RetailersOrder demoOrder);
+    @Delete("DELETE retailers_order WHERE id = #{retailersOrder.id}")
+    int delete(@Param("retailersOrder") RetailersOrder retailersOrder);
 
 
 }
